5b90e88574186a20af22eb79416df30c60aaf97c
Timothy Miller <tim.miller@timknowsbest.com>
2019-09-05 01:37:54 -0400

    optimized images

--- components/WelcomeBanner/WelcomeBanner.jsx
+++ components/WelcomeBanner/WelcomeBanner.jsx
@@ -2,16 +2,18 @@ import './style.less';
 import { Card } from 'antd';
 const { Meta } = Card;
 
+// const multipleSizes = require('/static/images/big-images-2.jpg?resize&sizes[]=240&sizes[]=600&sizes[]=1000');
+const oneSize = require('../../static/images/big-images-2.jpg?resize&size=240');
+
 const WelcomeBanner = (props) => {
-    
+
     return (<div className="WelcomeBanner">
         <Card
             hoverable
             style={{ width: 240 }}
-            cover={<img alt="example" src="/static/images/big-images-2.jpg
-            " />}
+            cover={<img alt="example" src={oneSize.src} />}
         >
-            {/* <Meta title="YouTube website" description="www.timknowsbest.com" /> */}
+            <Meta title="YouTube website" description="" />
         </Card>
 
     </div>);
