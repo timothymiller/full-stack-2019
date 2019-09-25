9ff26e3818343724e2ee49f0f99c7dd1ff954446
Timothy Miller <tim.miller@timknowsbest.com>
2019-09-25 19:38:19 -0400

    dotenv support for next.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const withOptimizedImages = require('next-optimized-images')
 // Service Worker for Progressive Web App (PWA) standards
 const withOffline = require('next-offline')
 
+// .env Environment variables
+require("dotenv").config();
+const webpack = require("webpack");
+
 // Bundle Analyzer
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 
@@ -107,6 +111,24 @@ module.exports = withBundleAnalyzer(withOffline(withOptimizedImages(withMDX(with
         use: 'null-loader',
       })
     }
+
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+    /**
+     * Returns environment variables as an object
+     */
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    /** Allows you to create global constants which can be configured
+    * at compile time, which in our case is our environment variables
+    */
+    config.plugins.push(new webpack.DefinePlugin(env));
+
     return config
   },
 }))))))
